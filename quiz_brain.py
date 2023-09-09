df7fd0218e3fe2cb4a41ee2d017ecf89fd798957
Aryabhatt <bhattsahab314@gmail.com>
2023-09-09 19:02:42 +0530

    Added a QuizBrain class consisting a constructor and a method called next_question()

---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+    def __init__(self, question_bank):
+        self.question_number = 0
+        self.question_list = question_bank
+
+    def next_question(self):
+        question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {question.text} (True/False)?: ")
+
