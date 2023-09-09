e291dd02e9495af3ed1422c03b1745c1e66f1190
Aryabhatt <bhattsahab314@gmail.com>
2023-09-09 19:13:42 +0530

    Added a basic loop to iterate over all the questions in main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for item in question_data:
     question = Question(item["text"], item["answer"])
     question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
