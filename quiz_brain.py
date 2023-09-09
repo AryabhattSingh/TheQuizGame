c751ee57ee0bf88562a885eef9fd5bada9cb3dc8
Aryabhatt <bhattsahab314@gmail.com>
2023-09-09 20:08:59 +0530

    Modeified next_question() so that user_input is validated for acceptable entries, added a check_answer() function to - check user answer, increment score, print right/wrong message

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,12 +1,29 @@
 class QuizBrain:
     def __init__(self, question_bank):
         self.question_number = 0
+        self.score = 0
         self.question_list = question_bank
 
     def next_question(self):
         question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {question.text} (True/False)?: ")
+        valid_user_input = False
+        while not valid_user_input:
+            user_answer = input(f"Q.{self.question_number}: {question.text} (True/False)?: ").capitalize()
+            if user_answer in ["True", "False", "T", "F"]:
+                valid_user_input = True
+            else:
+                print("\nKindly enter a valid entry.\n")
+        self.check_answer(user_answer, question)
+
+    def check_answer(self, user_answer, question):
+        if user_answer == question.answer or user_answer == question.answer[0]:
+            self.score += 1
+            print("\nYou got it.")
+        else:
+            print("\nWrong answer.")
+        print(f"The correct answer was: {question.answer}\nCurrent Score: "
+              f"{self.score}/{self.question_number}\n")
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
